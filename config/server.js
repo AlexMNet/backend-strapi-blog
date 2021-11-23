10a6729b156cac0149b65135e85d2391ba090da6
Alex Maldonado <alex@Alexs-MBP-3.home>
2021-11-23 02:50:17 -0800

    prep for deployment

--- config/server.js
+++ config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'b80264ec164a6f9bc13d1f79ba629971'),
+      secret: env("ADMIN_JWT_SECRET", "b80264ec164a6f9bc13d1f79ba629971"),
     },
   },
 });
