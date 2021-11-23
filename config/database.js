4a4f9bc2315a48bcbfb50c8e379257e844395e9c
Alex Maldonado <alex@Alexs-MBP-3.home>
2021-11-23 04:29:55 -0800

    update databse config

--- config/database.js
+++ config/database.js
@@ -1,14 +1,65 @@
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'bookshelf',
+//       settings: {
+//         client: 'sqlite',
+//         filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+//       },
+//       options: {
+//         useNullAsDefault: true,
+//       },
+//     },
+//   },
+// });
+
+// module.exports = ({ env }) => ({
+//   defaultConnection: "default",
+//   connections: {
+//     default: {
+//       connector: "mongoose",
+//       settings: {
+//         host: env("DATABASE_HOST"),
+//         srv: env.bool("DATABASE_SRV", true),
+//         port: env.int("DATABASE_PORT", 27017),
+//         database: env("DATABASE_NAME"),
+//         username: env("DATABASE_USERNAME"),
+//         password: env("DATABASE_PASSWORD"),
+//       },
+//       options: {
+//         authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
+//         ssl: env.bool("DATABASE_SSL", true),
+//       },
+//     },
+//   },
+// });
+
+// module.exports = ({ env }) => ({
+//   defaultConnection: "default",
+//   connections: {
+//     default: {
+//       connector: "mongoose",
+//       settings: {
+//         uri: env("DATABASE_URI"),
+//       },
+//       options: {
+//         ssl: true,
+//       },
+//     },
+//   },
+// });
+
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: "mongoose",
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        uri: env("DATABASE_URI"),
       },
       options: {
-        useNullAsDefault: true,
+        ssl: true,
       },
     },
   },
